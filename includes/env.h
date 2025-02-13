e7c54ff603e0df88cdcc96bc4893a0634127dc35
Alienxbe <martin.ykman@gmail.com>
2025-02-13 17:25:58 +0100

    Added new functions to env: - env_get_var - env_get_va_content

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: marykman <marykman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:36:57 by marykman          #+#    #+#             */
-/*   Updated: 2025/02/12 17:13:14 by marykman         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:34:15 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@
 
 // Functions
 /*
-	env_get_content_by_name(char *name)
-	env_replace_by_name(char *name)
+	env_get_content_by_name(const char *name)
+	env_replace_by_name(const char *name)
 	env_
 */
+
+t_list	*env_get_var(t_list *envl, const char *name);
+char	*env_get_var_content(t_list *envl, const char *name);
 
 // Utils
 t_list	*strs_to_lst(char **strs);
 char	**lst_to_strs(t_list *lst);
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpramann <vpramann@student.s19.be>         +#+  +:+       +#+        */
+/*   By: marykman <marykman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 22:12:47 by marykman          #+#    #+#             */
-/*   Updated: 2025/02/10 16:59:26 by vpramann         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:52:27 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <readline/history.h>
 #include "ft_printf.h"
 #include "parsing.h"
+#include "env.h"
 #include "minishell.h"
 
 static void	print_token(void *content)
@@ -55,15 +56,21 @@ int main(int argc, char const **argv, char **envp)
 {
 	char	*rl;
 	t_list	*cmds;
+	t_list	*envl;
 	int i = 0;
+	char	*prompt;
 
 	(void)argc;
 	(void)argv;
 	(void)envp;
+	envl = strs_to_lst(envp);
+	// printf("%s\n", env_get_var_content(envl, "PWD"));
 	// repl -> read eval print loop
 	while (i == 0)
 	{
-		rl = readline(PROMPT_STR);
+		prompt = ft_strjoinx(3, "Minishesh > ", env_get_var_content(envl, "PWD"), " $ ");
+		rl = readline(prompt);
+		free(prompt);
 		if (!rl)
 			exit(EXIT_FAILURE);
 		cmds = parsing(rl); // return a linked list of t_cmd
