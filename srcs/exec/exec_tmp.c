4b5d8fbd88f9d9d9990c1d5f6055f9fc8e33f8bd
Victor Pramann <vpramann@mi-r3-p4.s19.be>
2025-03-12 18:19:22 +0100

    un peu d'exec

--- srcs/exec/exec_tmp.c
+++ srcs/exec/exec_tmp.c
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec.c                                             :+:      :+:    :+:   */
+/*   exec_tmp.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpramann <vpramann@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 17:39:58 by vpramann          #+#    #+#             */
-/*   Updated: 2025/02/12 22:23:11 by vpramann         ###   ########.fr       */
+/*   Updated: 2025/03/12 17:41:06 by vpramann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parsing.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include "ft_memory.h"
+#include "exec.h"
+#include "env.h"
 
 void	free_tab(char **tab)
 {
@@ -102,7 +107,7 @@ char **find_cmds(t_list *cmds)
     return (cmdss);
 }
 
-void exec_cmd(t_list *cmds, char **envp)
+void exec_cmdtmp(t_list *cmds, char **envp)
 {
     char	**cmdss;
 	char	*path;
@@ -121,7 +126,7 @@ void exec_cmd(t_list *cmds, char **envp)
 	free_tab(cmdss);
 }
 
-void exec_cmds(t_list *cmds, char ** envp)
+void exec_cmdstmp(t_list *cmds, char ** envp)
 {
 	t_cmd *cmd;
 	int pipe_fds[2];
@@ -180,3 +185,126 @@ void exec_cmds(t_list *cmds, char ** envp)
 	close(in);
 	close(out);
 }	
+
+
+static int	(*init_pipes(t_list *cmds))[2]
+{
+	int	(*pipes)[2];
+	int	pipe_count;
+	int	i;
+
+	pipe_count = ft_lstsize(cmds);
+	if (pipe_count < 2)
+		return (NULL);
+	pipes = ft_calloc(pipe_count - 1, sizeof(pipes));
+	if (!pipes)
+		return (NULL);
+	i = -1;
+	while (++i < pipe_count - 1)
+		if (pipe(pipes[i]) < 0)
+			return (NULL);	// TODO: free
+	return (pipes);
+}
+
+static void	set_pipes(t_list *redir, int cmd_index, int (*pipes)[2])
+{
+	t_redir pot_file;
+	t_redir_type red_type;
+	int fd;
+	
+	// if redir_file
+	pot_file = redir->content;
+	red_type = NULL;
+	fd = -1;
+	if (pot_file.filename)
+	{
+		if (pot_file.type)
+			red_type = pot_file.type;
+		if (red_type == REDIR_STDIN)
+		{
+			if (access(pot_file.filename, F_OK) == 0)
+			{
+				if (access(pot_file.filename, R_OK) == 0)
+					fd = open(pot_file.filename, O_RDONLY, 0444);
+			/*else
+				print_errors(3, NULL, file1);*/
+			}
+			if (fd == -1)
+				exit (1);
+			dup2(fd, STDIN_FILENO);
+			dup2(pipes[0][1], STDOUT_FILENO);	
+		}
+		/*else if (red_type == REDIR_STDOUT)
+		{
+			
+		}*/
+	}
+	// else redir pipe
+	else
+		dup2(pipes[0][0], STDIN_FILENO);
+	if (cmd_index != 0)
+		dup2(pipes[cmd_index - 1][0], STDIN_FILENO);
+	if (cmd_index)
+	dup2(pipes[cmd_index][1], STDOUT_FILENO);
+}
+
+
+static void exec(t_cmd *cmd, t_list *envl)
+{
+	char	**to_ex;
+	char	*path;
+	char	**env_to_ex;
+	
+	//if (isbuiltin(tcmd))
+	// ftbuiltin
+	to_ex = lst_to_strs(cmd->tokens);
+	env_to_ex = lst_to_strs(envl);
+	path = find_cmd_path(to_ex[0], env_to_ex);
+	if (execve(path, to_ex, env_to_ex) == -1)
+	{
+		free(path);
+		free_tab(to_ex);
+		free_tab(env_to_ex);
+		return ;
+	}
+	free(path);
+	free_tab(to_ex);
+	free_tab(env_to_ex);
+}
+
+static void	exec_cmd(t_cmd *cmd, t_list *cmds,int cmd_index, t_list *envl)
+{
+	int	saved_io[2];
+
+	
+	(void)envl;
+	saved_io[0] = dup(STDIN_FILENO);
+	saved_io[1] = dup(STDOUT_FILENO);
+	// Setup pipes (redirect or pipe) -> set stdin/out
+	set_pipes(cmd->redirs, cmd_index, saved_io);
+	// Exec (builtins or not)
+	exec(cmd, envl);
+	dup2(saved_io[0], STDIN_FILENO);
+	dup2(saved_io[1], STDOUT_FILENO);
+	close(saved_io[0]);
+	close(saved_io[1]);
+}
+
+void	exec_cmds(t_list *cmds, t_list *envl)
+{
+	t_list	*lst;
+	int		(*pipes)[2];
+	int		i;
+
+	(void)envl;
+	pipes = init_pipes(cmds);
+	// Loop exec
+	lst = cmds;
+	i = 0;
+	while (lst)
+	{
+		exec_cmd(lst->content, cmds, i++, envl);
+		lst = lst->next;
+	}
+	free(pipes);
+}
