aec98c4710cf5cd678911b6cd44dfe571a50cf3a
Victor Pramann <vpramann@mi-r2-p2.s19.be>
2025-03-15 00:36:12 +0100

    un peu d'exec

--- includes/exec.h
+++ includes/exec.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marykman <marykman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vpramann <vpramann@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 21:34:21 by marykman          #+#    #+#             */
-/*   Updated: 2025/03/06 21:36:05 by marykman         ###   ########.fr       */
+/*   Updated: 2025/03/15 00:33:45 by vpramann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 # include "ft_list.h"
 
-void	exec_cmds(t_list *cmds, t_list *envl);
+void        exec_cmds(t_list *cmds, t_list *envl);
+static void exec_cmd(t_cmd *cmd,int cmd_index, t_list *envl);
+static void exec(t_cmd *cmd, t_list *envl);
+static void	set_pipes(t_list *redir, int cmd_index, int (*pipes)[2]);
+static int	(*init_pipes(t_list *cmds))[2];
+static char	*find_path(char **paths, char **cmds, int i);
 
 #endif
