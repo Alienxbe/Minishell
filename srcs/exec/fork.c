5650049535c942e3583f8beced994a4ff0cf54e8
victor pramann <victor@victors-MacBook.local>
2025-05-03 12:59:38 +0200

    absolute and relative path check + bit of error handling

--- srcs/exec/fork.c
+++ srcs/exec/fork.c
@@ -28,9 +28,28 @@ void    child_process(t_cmd *cmd, char **envc, int (*pipes)[2], int nb_cmds)
     to_ex = lst_to_strs(cmd->tokens);
    if (!to_ex || !to_ex[0])
         return ;
-    path = find_cmd_path(to_ex[0], envc);
+    if (has_absolute_path(to_ex[0]) || has_relative_path(to_ex[0]))
+    {
+        if (access(to_ex[0], F_OK | X_OK) == 0)
+            path = ft_strdup(to_ex[0]);
+        else
+        {
+            printf("minishell: %s: command not found\n", to_ex[0]);
+            free_tab(to_ex);
+            free_tab(envc);
+            exit(127);
+        }
+    }
+    else
+        path = find_cmd_path(to_ex[0], envc);
     if (execve(path, to_ex, envc) == -1)
+    {
+        perror("execve");
+        free(path);
+        free_tab(to_ex);
+        free_tab(envc);
         exit(1);
+    }
     free(path);
     free_tab(to_ex);
     free_tab(envc);
