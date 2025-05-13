80be2be416580fcf0cf5ede112dc185359648afc
Victor Pramann <vpramann@fu-c4-p4.s19.be>
2025-05-13 17:27:23 +0200

    pipe fixing (wip)

--- srcs/exec/fork.c
+++ srcs/exec/fork.c
@@ -6,7 +6,7 @@
 /*   By: vpramann <vpramann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 16:16:19 by vpramann          #+#    #+#             */
-/*   Updated: 2025/05/12 16:07:28 by vpramann         ###   ########.fr       */
+/*   Updated: 2025/05/13 16:52:28 by vpramann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ void	child_process(t_cmd *cmd, char **envc, int (*pipes)[2], int nb_cmds)
 	start_signals_exec();
     (void)pipes;
     (void)nb_cmds;
+	
 	close_pipes(pipes, nb_cmds);
 	to_ex = lst_to_strs(cmd->tokens);
 	if (!to_ex || !to_ex[0])
@@ -64,7 +65,7 @@ void	child_process(t_cmd *cmd, char **envc, int (*pipes)[2], int nb_cmds)
 	}
 	else
 		path = find_cmd_path(to_ex[0], envc);
-	if (!path)
+	if (!path || access(path, F_OK | X_OK) != 0)
 		return (printf("minishell: %s: command not found\n", to_ex[0]),
 			free_tab(to_ex), free_tab(envc), exit(127));
 	if (execve(path, to_ex, envc) == -1)
