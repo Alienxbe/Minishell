0c645715c92b0815b5230d01110f4d68ee7367e3
Victor Pramann <vpramann@shi-r8-p2.s19.be>
2025-05-17 16:05:07 +0200

    norme

--- srcs/exec/fork.c
+++ srcs/exec/fork.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   fork.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vpramann <vpramann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 16:16:19 by vpramann          #+#    #+#             */
-/*   Updated: 2025/05/17 03:44:52 by victor           ###   ########.fr       */
+/*   Updated: 2025/05/17 16:04:30 by vpramann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+int	get_exit_status(int *status)
+{
+	int		ret;
+
+	if (WIFEXITED(*status))
+		ret = WEXITSTATUS(*status);
+	else if (WIFSIGNALED(*status))
+	{
+		if (WTERMSIG(*status) == SIGINT)
+			write(1, "\n", 1);
+		ret = 128 + WTERMSIG(*status);
+	}
+	return (ret);
+}
 
 void	parent_process(t_list **pids, t_cmd_table *cmd_table)
 {
@@ -30,18 +45,7 @@ void	parent_process(t_list **pids, t_cmd_table *cmd_table)
 			return ;
 		pid = *(long *)(tmp);
 		waitpid(pid, &status, 0);
-		if (WIFSIGNALED(status))
-        {
-            if (WTERMSIG(status) == SIGINT)
-            {
-                write(1, "\n", 1);
-                last_ret = 130;
-            }
-            else
-                last_ret = 128 + WTERMSIG(status);
-        }
-        else if (WIFEXITED(status))
-            last_ret = WEXITSTATUS(status);
+		last_ret = get_exit_status(&status);
 		tmp = tmp->next;
 	}
 	cmd_table->last_ret = last_ret;
@@ -50,77 +54,21 @@ void	parent_process(t_list **pids, t_cmd_table *cmd_table)
 	start_signals();
 }
 
-/*void	parent_process(t_list **pids)
-{
-	long				pid;
-	t_list				*tmp;
-	int					status;
-	struct sigaction	sa_old;
-
-	sigaction(SIGINT, &(struct sigaction){.sa_handler = SIG_IGN}, &sa_old);
-	if (!pids || !*pids)
-		return ;
-	tmp = *pids;
-	while (tmp)
-	{
-		if (!tmp->content)
-			return ;
-		pid = *(long *)(tmp);
-		waitpid(pid, &status, 0);
-		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-			write(1, "\n", 1);
-		tmp = tmp->next;
-	}
-	free_pids(pids);
-	sigaction(SIGINT, &sa_old, NULL);
-	start_signals();
-}*/
-
-
-/*void	child_process(t_cmd *cmd, char **envc, int (*pipes)[2], int nb_cmds)
-{
-	char	**to_ex;
-	char	*path;
-
-	start_signals_exec();
-    (void)pipes;
-    (void)nb_cmds;
-	
-	close_pipes(pipes, nb_cmds);
-	to_ex = lst_to_strs(cmd->tokens);
-	if (!to_ex || !to_ex[0])
-	{
-		free_tab(to_ex);
-        free_tab(envc);
-        exit(1);
-	}
-	if (has_absolute_path(to_ex[0]) || has_relative_path(to_ex[0]))
-	{
-		if (access(to_ex[0], F_OK | X_OK) == 0)
-			path = ft_strdup(to_ex[0]);
-		else
-			return (printf("minishell: %s: command not found\n", to_ex[0]),
-				free_tab(to_ex), free_tab(envc), exit(127));
-	}
-	else
-		path = find_cmd_path(to_ex[0], envc);
-	if (!path || access(path, F_OK | X_OK) != 0)
-		return (printf("minishell: %s: command not found\n", to_ex[0]),
-			free_tab(to_ex), free_tab(envc), exit(127));
-	if (execve(path, to_ex, envc) == -1)
-		return (perror("execve"), free(path),
-			free_tab(to_ex), free_tab(envc), exit(1));
-	free(path);
-	free_tab(to_ex);
-	free_tab(envc);
-	exit(0);
-}*/
-
 void	exit_child_process(char **to_ex, char **envc)
 {
 	free_tab(to_ex);
 	free_tab(envc);
 	exit(1);
+}
+
+void	access_program(char *path, char **to_ex, char **envc)
+{
+	if (!path || access(path, F_OK | X_OK) != 0)
+		return (printf("minishell: %s: command not found\n", to_ex[0]),
+			free_tab(to_ex), free_tab(envc), exit(127));
+	if (access(path, F_OK) == 0 && access(path, X_OK) != 0)
+		return (printf("minishell: %s: permission denied\n", to_ex[0]),
+			free(path), free_tab(to_ex), free_tab(envc), exit(126));
 }
 
 void	child_process(t_cmd *cmd, char **envc, int (*pipes)[2], int nb_cmds)
@@ -143,12 +91,7 @@ void	child_process(t_cmd *cmd, char **envc, int (*pipes)[2], int nb_cmds)
 	}
 	else
 		path = find_cmd_path(to_ex[0], envc);
-	if (!path || access(path, F_OK | X_OK) != 0)
-		return (printf("minishell: %s: command not found\n", to_ex[0]),
-			free_tab(to_ex), free_tab(envc), exit(127));
-	if (access(path, F_OK) == 0 && access(path, X_OK) != 0)
-		return (printf("minishell: %s: permission denied\n", to_ex[0]),
-			free(path), free_tab(to_ex), free_tab(envc), exit(126));
+	access_program(path, to_ex, envc);
 	execve(path, to_ex, envc);
 	return (perror("execve"), free(path),
 		free_tab(to_ex), free_tab(envc), exit(1));
