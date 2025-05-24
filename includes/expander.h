4ff04ef4c6a536d2945b564ea2e90d2815fc52d7
Alienxbe <martin.ykman@gmail.com>
2025-05-24 20:57:59 +0200

    Cleaning quote checking in token generation

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 16:25:54 by marykman          #+#    #+#             */
-/*   Updated: 2025/05/24 10:06:08 by marykman         ###   ########.fr       */
+/*   Updated: 2025/05/24 19:47:21 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define EXPANDER_H
 
 #include "parsing.h"
+
+typedef enum e_expander_error
+{
+	EXPANDER_SUCCESS,
+	EXPANDER_QUOTING_ERROR
+}	t_expander_error;
 
 char	*expand(char *token, t_list *envl);
 char	*ft_substr_expand(char *s, unsigned int start, size_t len,
--- srcs/parsing/token.c
+++ srcs/parsing/token.c
@@ -6,7 +6,7 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 09:35:13 by marykman          #+#    #+#             */
-/*   Updated: 2025/05/24 16:16:52 by marykman         ###   ########.fr       */
+/*   Updated: 2025/05/24 20:24:52 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,17 @@ static int	is_token_delimiter(const char c)
 	return (ft_isspace(c) || ft_strchr(TOKEN_DELIMITER, c));
 }
 
-static void	skip_quotes(const char *input, size_t *len)
+static t_expander_error	skip_quotes(const char *input, size_t *len)
 {
 	char	*next;
 
 	if (!ft_strchr(QUOTE_TYPES, input[*len]))
-		return ;
+		return (EXPANDER_SUCCESS);
 	next = ft_strchr(input + *len + 1, input[*len]);
 	if (!next)
-	{
-		ft_printf("Quoting error\n");
-		(*len)++;
-		return ; // 	EXIT
-	}
-	(*len) += next - input;
+		return (EXPANDER_QUOTING_ERROR);
+	(*len) = next - input;
+	return (EXPANDER_SUCCESS);
 }
 
 char *get_token(const char *input, size_t *pos, t_list *envl)
@@ -45,7 +42,8 @@ char *get_token(const char *input, size_t *pos, t_list *envl)
 	len = 0;
 	while (input[*pos + len] && !is_token_delimiter(input[*pos + len]))
 	{
-		skip_quotes(input + *pos, &len);
+		if (skip_quotes(input + *pos, &len) == EXPANDER_QUOTING_ERROR)
+			return (NULL);
 		len++;
 	}
 	token = ft_substr(input, *pos, len);
